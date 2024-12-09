d6c54dad99d11827b316194cde74a119d3c6f489
Ainun Nadhifah Syamsiyah <58164571+ainunns@users.noreply.github.com>
2024-12-09 09:56:53 +0000

    feat: implement 3D text rendering with TextMesh component

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,16 @@
-import './App.css';
+import { Canvas } from '@react-three/fiber';
 
-import { useState } from 'react';
+import TextMesh from './components/TextMesh';
 
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
-
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <pointLight position={[10, 10, 10]} />
+      <TextMesh position={[-2, 0, 0]} text='N' />
+      <TextMesh position={[2, 0, 0]} text='3' />
+    </Canvas>
   );
-}
+};
 
 export default App;
---
+++ src/components/TextMesh.tsx
@@ -0,0 +1,28 @@
+import { useLoader } from '@react-three/fiber';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+
+interface TextMeshProps {
+  position: [number, number, number];
+  text: string;
+}
+
+export default function TextMesh({ position, text }: TextMeshProps) {
+  const font = useLoader(
+    FontLoader,
+    'https://threejs.org/examples/fonts/helvetiker_regular.typeface.json',
+  );
+  const textOptions = {
+    font,
+    size: 1,
+    height: 0.2,
+  };
+
+  const textGeometry = new TextGeometry(text, textOptions);
+
+  return (
+    <mesh position={position} geometry={textGeometry}>
+      <meshStandardMaterial color='black' />
+    </mesh>
+  );
+}
